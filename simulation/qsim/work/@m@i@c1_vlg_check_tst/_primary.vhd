e5233d942cf239b4f5348fd4ce168ca1103589fd
Hugo Henrique <hugohmarques4@gmail.com>
2025-04-12 18:02:07 -0300

    Modificação no FULL_ADDER1bit

--- simulation/qsim/work/@m@i@c1_vlg_check_tst/_primary.vhd
+++ simulation/qsim/work/@m@i@c1_vlg_check_tst/_primary.vhd
@@ -2,10 +2,8 @@ library verilog;
 use verilog.vl_types.all;
 entity MIC1_vlg_check_tst is
     port(
-        O0              : in     vl_logic;
-        O1              : in     vl_logic;
-        O2              : in     vl_logic;
-        O3              : in     vl_logic;
+        COUT            : in     vl_logic;
+        O               : in     vl_logic;
         sampler_rx      : in     vl_logic
     );
 end MIC1_vlg_check_tst;
