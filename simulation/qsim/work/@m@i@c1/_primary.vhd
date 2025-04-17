7a327e0d82e8a2ab1ed7d06b176b818f4df6ff4c
Hugo Henrique <hugohmarques4@gmail.com>
2025-04-17 12:09:34 -0300

    ULA_1bit

--- simulation/qsim/work/@m@i@c1/_primary.vhd
+++ simulation/qsim/work/@m@i@c1/_primary.vhd
@@ -2,11 +2,15 @@ library verilog;
 use verilog.vl_types.all;
 entity MIC1 is
     port(
-        \00\            : out    vl_logic;
-        I0              : in     vl_logic;
-        I1              : in     vl_logic;
-        \01\            : out    vl_logic;
-        \02\            : out    vl_logic;
-        \03\            : out    vl_logic
+        \Vai-um\        : out    vl_logic;
+        B               : in     vl_logic;
+        ENB             : in     vl_logic;
+        INVA            : in     vl_logic;
+        A               : in     vl_logic;
+        ENA             : in     vl_logic;
+        F0              : in     vl_logic;
+        F1              : in     vl_logic;
+        \Vem-um\        : in     vl_logic;
+        Saida           : out    vl_logic
     );
 end MIC1;
