1a41c96cbd13a6aedc7513cee5e95e56a7dc4c2b
Hugo Henrique Marques <56773808+hugnarok@users.noreply.github.com>
2025-05-20 08:38:52 -0300

    Delete simulation directory

--- simulation/qsim/work/@m@i@c1_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity MIC1_vlg_check_tst is
-    port(
-        COUT            : in     vl_logic;
-        O               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end MIC1_vlg_check_tst;
--- simulation/qsim/work/@m@i@c1_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity MIC1_vlg_vec_tst is
-end MIC1_vlg_vec_tst;
